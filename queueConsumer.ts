30c177046fa731f690af9842085ff9a0cde1432a
daichitakahashi <t.daichi.snd@gmail.com>
2024-07-23 01:57:45 +0900

    feat: implement QueueConsumer creation

--- queueConsumer.ts
+++ queueConsumer.ts
@@ -1,6 +1,7 @@
 import * as crypto from "node:crypto";
 import { HttpBody, HttpClientRequest } from "@effect/platform";
 import * as Http from "@effect/platform/HttpClient";
+import { Schema } from "@effect/schema";
 import type { CustomResourceOptions, Input } from "@pulumi/pulumi";
 import {
   type CreateResult,
@@ -36,7 +37,9 @@ export interface QueueConsumerArgs {
 }
 
 type QueueConsumerProviderArgs = unwrapInput<QueueConsumerArgs>;
-type QueueConsumerProviderState = QueueConsumerProviderArgs;
+type QueueConsumerProviderState = QueueConsumerProviderArgs & {
+  consumerId: string;
+};
 
 class QueueConsumerProvider
   implements
@@ -47,8 +50,13 @@ class QueueConsumerProvider
   async create(
     args: QueueConsumerProviderArgs,
   ): Promise<CreateResult<QueueConsumerProviderState>> {
-    // FIXME: implement
-    return { id: crypto.randomUUID(), outs: args };
+    const { consumer_id } = await E.runPromise(
+      createQueueConsumer(this.apiToken, args),
+    );
+    return {
+      id: crypto.randomUUID(),
+      outs: { ...args, consumerId: consumer_id },
+    };
   }
 
   async diff(
@@ -78,6 +86,66 @@ class QueueConsumerProvider
     // FIXME: implement
   }
 }
+
+const params = (args: QueueConsumerProviderArgs) => {
+  const params: Record<
+    string,
+    string | Record<string, number | undefined> | undefined
+  > = {
+    type: "worker",
+    script_name: args.scriptName,
+    dead_letter_queue: args.deadLetterQueue,
+  };
+  if (
+    args.batchSize ||
+    args.maxRetries ||
+    args.maxWaitTimeMs ||
+    args.retryDelay ||
+    args.maxConsumerConcurrency
+  ) {
+    params.settings = {
+      batch_size: args.batchSize,
+      max_retries: args.maxRetries,
+      max_wait_time_ms: args.maxWaitTimeMs,
+      retry_delay: args.retryDelay,
+      max_concurrency: args.maxConsumerConcurrency,
+    };
+  }
+  return params;
+};
+
+const CreateQueueConsumerResponse = Schema.Struct({
+  consumer_id: Schema.String.pipe(Schema.nonEmpty()),
+});
+
+const createQueueConsumer = (
+  apiToken: string,
+  args: QueueConsumerProviderArgs,
+) =>
+  pipe(
+    HttpBody.json(params(args)),
+    E.flatMap((body) =>
+      E.scoped(
+        E.retry(
+          Http.fetchOk(
+            HttpClientRequest.post(
+              `https://api.cloudflare.com/client/v4/accounts/${encodeURIComponent(args.accountId)}/queues/${encodeURIComponent(args.queueId)}/consumers`,
+              {
+                headers: {
+                  Authorization: `Bearer ${apiToken}`,
+                  "Content-Type": "application/json",
+                },
+                body,
+              },
+            ),
+          ),
+          Schedule.addDelay(Schedule.recurs(5), () => "1 second"),
+        ),
+      ),
+    ),
+    E.flatMap((resp) => resp.json),
+    E.flatMap(Schema.decodeUnknown(CreateQueueConsumerResponse)),
+  );
 
 export class QueueConsumer extends Resource {
   constructor(
